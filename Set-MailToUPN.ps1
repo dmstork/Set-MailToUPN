c41d3a957cf527db9de73c7a70e474692b8a45c5
Dave Stork <35466207+dmstork@users.noreply.github.com>
2022-01-06 18:46:55 +0100

    Added File import capabilities, added comments, cleaned up code

--- Set-MailToUPN.ps1
+++ Set-MailToUPN.ps1
@@ -1,5 +1,13 @@
+# Simple script written by Dave Stork 
+# Mail: dmstork at stalpaert.nl
+# Twitter: @dmstork
 # 
-# Required imports Mailboxes.txt with SamAccountName as minimal input column
+# Provided as is, use at own risk.
+#
+# Imports CSV file with SamAccountName as minimal input column.
+# Changes current AD UPN to the primary SMTP address found in AD.
+# Saves original values before changing (can be used with Restore-MailToUPN.ps1)
+# Requires the AD PowerShell module
 # 
 
 # Definition of possible parameters and switches
@@ -7,16 +15,16 @@
 # InputFile is the CSV with accounts that have to be changed.
 Param(
     [switch]$TranscriptOn,
-    # Specifies a path to one or more locations.
-    [Parameter(Mandatory=$false,
+    # Specifies a filepath to CSV file with accounts to be changed. Mandatory
+    [Parameter(Mandatory=$True,
               ValueFromPipeline=$true,
               ValueFromPipelineByPropertyName=$true,
               HelpMessage="Path CSV file with accounts to be processed")]
-                [ValidateNotNullOrEmpty()]
-                [string]$CSVFile
+              [ValidateNotNullOrEmpty()]
+              [string]$CSVFile
 )
 
-# Check wether ActiveDirectory PowerShell module is installed
+# Check wether ActiveDirectory PowerShell module is installed and Imports module ifso
 $Check = Get-Command -Module ActiveDirectory
 If ($Null -eq $Check){
     Write-Output "Error: Active Directory PowerShell module is not installed on this machine. Stopping."
@@ -25,7 +33,7 @@ If ($Null -eq $Check){
     Import-Module ActiveDirectory
 }
 
-# Start Transcript if requested
+# Start Transcript if requested via switch
 If ($TranscriptOn -eq $true) {
     # Defining logtime variable to be used in logging/default output folder
     $LogTime = Get-Date -Format "yyyyMMdd_hhmm_ss"
@@ -44,9 +52,9 @@ Try {
     $Accounts = Import-CSV -Path $CSVFile -ErrorAction Continue
 } Catch {
     Write-Error "Failed to import CSV File" -ErrorAction Stop
-
 }
 
+# legacy import
 # $Accounts = Import-CSV -Path "Mailboxes.txt"
 
 #Defining array for Export
@@ -59,13 +67,16 @@ $CurrentNoPrimarySMTP = @()
 Write-Output "Current attribute values exported"
 # Write-Output "SamAccountName, UserPrincipalName, PrimarySMTPAddress"
 
+#Loop for handling each row ie account
 ForEach ($Account in $Accounts){
     $Identity = [String]$Account.SamAccountname
     $DistinguishedName = [String]$Account.DistinguishedName
 
-    # Write-Output "Identity is $Identity"
+    # Determining the Primary SMTP address from multiple values. It should start with capitol SMTP: and then the address.
     $PrimaryAddress = Get-ADUser -Identity $Identity -Properties ProxyAddresses | Select-Object -Expand proxyAddresses | Where-Object {$_ -clike "SMTP:*"}
     
+    # Check whether there is a PrimarySMTP address present.
+    # If this is the case, the current values are stored in array for later export
     If ($null -ne $PrimaryAddress){
         $PrimaryAddress = $PrimaryAddress.SubString(5)
         $UserPrincipalName = [String]$Account.UserPrincipalName
@@ -81,7 +92,7 @@ ForEach ($Account in $Accounts){
         $CurrentAccounts += $CurrentUser
 
     } else {
-
+        # When no Primary SMTP address is found, the values are stored in another array for later export
         $UserPrincipalName = [String]$Account.UserPrincipalName
             
         $NoSMTPUser = [PSCustomObject] @{
@@ -95,6 +106,7 @@ ForEach ($Account in $Accounts){
    
 }
 
+# Now we are going to actually change the UPN of the accounts that have a Primary SMTP
 Write-Output "Changing UPN to Primary SMTP"
 
 $ChangedAccounts = @()
@@ -102,9 +114,14 @@ $ChangedAccounts = @()
 ForEach ($Account in $Accounts){
     $Identity = [String]$Account.SamAccountname
     $DistinguishedName = [String]$Account.DistinguishedName
+    
+    # Determining the Primary SMTP address from multiple values. It should start with capitol SMTP: and then the address.
     $PrimaryAddress = Get-ADUser -Identity $Identity -Properties ProxyAddresses | Select-Object -Expand proxyAddresses | Where-Object {$_ -clike "SMTP:*"}
     
+    # Check to see if there is a Primary SMTP present
     If ($null -ne $PrimaryAddress){
+        
+        #Primary SMTP is created by omitting the first 5 characters (SMTP:)
         $PrimaryAddress = $PrimaryAddress.SubString(5)
         $UserPrincipalName = [String]$Account.UserPrincipalName
         
@@ -116,7 +133,7 @@ ForEach ($Account in $Accounts){
             Write-Output "Error with $Identity and $PrimaryAddress "
         }
 
-        # Registering what has been changed
+        # Registering what has been changed in this account, saved in array
         $UserPrincipalName = (Get-ADUser -Identity $Identity).UserPrincipalName
         $ChangedUser = [PSCustomObject] @{
             SamAccountname = $Identity
@@ -139,15 +156,16 @@ ForEach ($Account in $Accounts){
 #Get date for export
 $LogTime = Get-Date -Format "yyyyMMdd"
 
-#Export to files
-$CurrentAccounts | Export-CSV -NoTypeInformation -Path $logtime"CurrentAccounts.txt"
+#Exporting all arrays to CSV files
+$CurrentAccounts | Export-CSV -NoTypeInformation -Path $logtime"OriginalStateAccounts.txt"
 $CurrentNoPrimarySMTP | Export-CSV -NoTypeInformation -Path $logtime"CurrentNoPrimarySMTP.txt"
 $ChangedAccounts | Export-CSV -NoTypeInformation -Path $logtime"ChangedAccounts.txt"
 
 
-# Clean up 
+# Clean up PS Modules
 Remove-Module ActiveDirectory
 
+# Turn of Transcript
 If ($TranscriptOn -eq $true) {
     Stop-Transcript
 }
